9eb0f8579ff34bac121ac97369c1e7ac8060db00
Brandon Tao <btao@csm-pm-38123-17.local>
2013-05-07 16:02:16 -0700

    Updated GitHub

--- Week4/electrostatics.py
+++ Week4/electrostatics.py
@@ -8,3 +8,8 @@ def dipolePotential(x,y,q,d):
 	"""Take point(s), the size and direction of charge, and the position of that charge to return the dipole potential due to that charge."""
 	Vxy = pointPotential(x,y,q,-d,0.)-pointPotential(x,y,q,d,0.)
 	return Vxy
+
+def pointField(x,y,q,Xq,Yq):
+	"""Takes as arguments the arrays (x,y), charge (q), and position (Xq, Yq), and returns the electric field components (Ex, Ey)."""
+	Ex = (k*q*(x-Xq))/(((x-Xq)**2)+((y-Yq)**2)**(1/2))
+	Ey = (k*q*(y-Yq))/(((x-Xq)**2)+((y-Yq)**2)**(1/2))
